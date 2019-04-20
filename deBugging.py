b8c86ac231937f1eb71724f06933a21b4621a1b6
Rahul Verma <rahul.verma04@sap.com>
2019-04-20 17:48:51 +0530

    Used pdb for debugging

---
+++ deBugging.py
@@ -0,0 +1,14 @@
+import pdb
+
+def make_bread():
+	pdb.set_trace()
+	return "I  dont have time"
+
+print(make_bread())
+
+#as soon as you exceute this program you enter 
+#the pdb debugger
+#commands c:continue execution
+# w:shows the context of the current line it is executing
+#a: print the arg list of current function
+#etc... 
